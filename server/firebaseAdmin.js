26d3937b6a5e9015f742d0ec0fc43ebf55a06ec7
Agniva-07 <haitagniva@gmail.com>
2026-05-23 01:37:01 +0530

    fix firebase pem parsing on render

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -1,11 +1,17 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(
+let serviceAccount = JSON.parse(
   process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// Fix Render newline issue
+serviceAccount.private_key =
+  serviceAccount.private_key.replace(/\\n/g, "\n");
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 module.exports = admin;
